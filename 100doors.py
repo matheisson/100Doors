47b99b0a9642dfeb85254241eac2c395c044f0ad
matheisson <csanyil@gmail.com>
2016-05-17 10:23:35 +0200

    codecool

---
+++ 100doors.py
@@ -0,0 +1,9 @@
+#05.17 100 doors project
+#Crea.Com // Csányi L.
+
+print("Open doors:")
+
+#write out square numbers, as these are the solutions
+for i in range(1,11):
+	square = i*i
+	print(square)
